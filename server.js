4c2eb8bd6ddf7533329ed595921f6463df413894
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-01-10 20:04:12 -0300

    fix path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Path = require('path');
+const Path = require('path');
 const InitServer = require('./initServer');
-console.log(Path.join(__dirname,'./project'));
+
 const initServer = new InitServer({
         port: parseInt(process.env.PORT) || 3000,
         host: process.env.HOST || '0.0.0.0',
